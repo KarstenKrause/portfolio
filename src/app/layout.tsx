69a67aea29c3aec2c9535da88f2adb5b8f84af99
Karsten Krause <karsten.krause1988@gmail.com>
2024-06-21 22:24:21 +0200

    set up project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
+import InstallBootstrap from "./components/InstallBootstrap";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +18,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <InstallBootstrap />
+        {children}
+      </body>
     </html>
   );
+  
 }
